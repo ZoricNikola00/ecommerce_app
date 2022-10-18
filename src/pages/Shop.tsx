c3d6af129d963d59dad7398d105af4b98405dd33
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-18 18:35:22 +0200

    Creating and editing Shop page

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import {useQuery} from '@tanstack/react-query'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
+import { prodType } from '../context'
+import Item from '../components/Item'
+
+const fetchData=async(s:string|undefined)=>{
+    let data
+    if(s==='all'){
+     data=await axios.get(`https://fakestoreapi.com/products/`)
+    }
+    else{
+     data=await axios.get(`https://fakestoreapi.com/products/category/${s}`)
+}
+return data?.data
+}
 
 const Shop = () => {
+    const {string}=useParams()
+    const {data,isLoading}=useQuery(['shop',string],()=>fetchData(string))
+    console.log(data)
   return (
-    <div>Shop</div>
+    <div className='prod'>
+            <h1>{string}</h1>
+            <div className='prod-wrap'>
+          {data?.map((x:prodType)=>{
+            const {id,image,title,price}=x
+            return <Item key={id} item={x}/>
+          })}
+          </div>
+        </div>
   )
 }
 
