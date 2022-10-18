6b9f3508b156315cbed1f08e01d82e5e8309d8df
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-18 22:56:20 +0200

    Creating Sign in from and editing

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React,{useRef} from 'react'
 
 const SignIn = () => {
+  const username:React.MutableRefObject<any>=useRef(null)
+  const password:React.MutableRefObject<any>=useRef(null)
+  const login=(e:any)=>{
+    e.preventDefault()
+    const data={
+      username:username.current.value,
+      password:password.current.value
+    }
+    const json=JSON.stringify(data, null, 4);
+    console.log(json)
+  }
   return (
-    <div>SignIn</div>
+      <form onSubmit={login}>
+        <h1>Sign In</h1>
+        <label>Username/Email</label>
+        <input placeholder='Type your username or email' type='text' ref={username} required />
+        <label>Password</label>
+        <input placeholder='Type your password' type='password' ref={password} required/>
+        <button type='submit'>Login</button>
+      </form>
   )
 }
 
