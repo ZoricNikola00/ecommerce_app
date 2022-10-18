bd6fa67ea657ab8abb3af23d5519baa1bce1096c
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-18 18:33:56 +0200

    Creating and editing Specific page

--- src/pages/Specific.tsx
+++ src/pages/Specific.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { prodType, useGlobalContext } from '../context'
 
 const Specific = () => {
+  const {products}=useGlobalContext()
+  const {id}=useParams()
+  const itemEl=products?.find((x:prodType)=>x.id===Number(id))
   return (
-    <div>Specific</div>
+    <div className='specific'>
+      <img src={itemEl?.image}/>
+      <div className='specific-info'>
+        <span>{itemEl?.category}</span>
+        <h1>{itemEl?.title}</h1>
+        <p>Rating {itemEl?.rating?.rate}</p>
+        <h2>$ {itemEl?.price}</h2>
+        <p>{itemEl?.description}</p>
+        <button>Add To Cart</button>
+      </div>
+    </div>
   )
 }
 
