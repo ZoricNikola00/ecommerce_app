11021cb274496452ddc49cfd675a161136a47e9d
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-18 19:57:13 +0200

    Adding items to Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import { useGlobalContext } from '../context'
-import {FiShoppingCart} from 'react-icons/fi'
+import { prodType, useGlobalContext } from '../context'
+import {FiShoppingCart,FiTrash2} from 'react-icons/fi'
 const Cart = () => {
-    const {openCart,setOpenCart}=useGlobalContext()
+    const {state,openCart,setOpenCart,del,plusminus}=useGlobalContext()
     
 
   return (
     <>
-    <div className='cart' onClick={()=>setOpenCart(true)}><FiShoppingCart/><div>1</div></div>
+    <div className='cart' onClick={()=>setOpenCart(true)}><FiShoppingCart/><div>{state.amount}</div></div>
     <div className={`${openCart?'cartBackground active':'cartBackground'}`}></div>
         <div className={`${openCart?'cartContainer active':'cartContainer'}`}>
-                <div onClick={()=>setOpenCart(false)}>X</div>
-             
+          <div className='x' onClick={()=>setOpenCart(false)}>X</div>
+          {state.cart.length<1?<div className='cartItem'>Cart Is Empty!</div>:state.cart.map((x:prodType)=>{
+            return <div className='cartItem'>
+              <img src={x.image}/>
+              <p>{x.title}</p>
+              <div>
+                <button onClick={()=>plusminus(x.id,'MINUS')}>-</button>
+                <span>{x.amount}</span>
+                <button onClick={()=>plusminus(x.id,'PLUS')}>+</button>
+              </div>
+              <div>$ {x.amount*x.price}</div>
+              <button onClick={()=>del(x.id)}><FiTrash2/></button>
+              </div>
+          })}
+          <h1 className='total'>Total price is: $ {state.total}</h1>  
         </div>
     </>
   )
