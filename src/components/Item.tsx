9b6845db767cab063384873e0eec69bb66961d37
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-18 19:57:39 +0200

    Minor fix

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,14 +2,15 @@ import React,{useState} from 'react'
 import { prodType, useGlobalContext } from '../context'
 import {Link} from 'react-router-dom'
 
-const Item = ({id,title,price,image}:prodType) => {
+const Item = ({item}:any) => {
     const [view,setView]=useState(false)
-    const {setOpenCart}=useGlobalContext()
+    const {setOpenCart,addToCart}=useGlobalContext()
+    const {id,title,image,price}=item
   return (
     <div onMouseEnter={()=>setView(true)} onMouseLeave={()=>setView(false)} className='item'>
         <div className={`${view?'viewMore active':'viewMore'}`}>
             <Link to={`/specific/${id}`}><button>View More</button></Link>
-            <button onClick={()=>setOpenCart(true)}>Add To Cart</button>
+            <button onClick={()=>{setOpenCart(true);addToCart(id,item)}}>Add To Cart</button>
           </div>
         <img src={image}/>
         <div className="item-info">
